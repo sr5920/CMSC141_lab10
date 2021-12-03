49ab6d6b76fa55e52b0e450a0ce2fb2ab5d3ee9f
sr5920 <95500808+sr5920@users.noreply.github.com>
2021-12-03 15:30:35 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,15 +1,50 @@
-function setup(){
-  var foo = new p5.Speech(); // speech synthesis object
-foo.speak('hi there'); // say something
-}
+var myRec = new p5.SpeechRec('en-US', parseResult); // new P5.SpeechRec object
+	myRec.continuous = true; // do continuous recognition
+	myRec.interimResults = true; // allow partial recognition (faster, less accurate)
 
-function draw(){
-  var foo = new p5.SpeechRec(); // speech recognition object (will prompt for mic access)
-foo.onResult = showResult; // bind callback function to trigger when speech is recognized
-foo.start(); // start listening
-}
+	var x, y;
+	var dx, dy;
 
-function showResult()
-{
-  console.log(foo.resultString); // log the result
-}
+	function setup()
+	{
+		// graphics stuff:
+		createCanvas(800, 600);
+		background(255, 255, 255);
+		fill(0, 0, 0, 255);
+		x = width/2;
+		y = height/2;
+		dx = 0;
+		dy = 0;
+
+		// instructions:
+		textSize(20);
+		textAlign(LEFT);
+		text("draw: up, down, left, right, clear", 20, 20);
+
+		//myRec.onResult = parseResult; // now in the constructor
+		myRec.start(); // start engine
+	}
+
+	function draw()
+	{
+		ellipse(x, y, 5, 5);
+		x+=dx;
+		y+=dy;
+		if(x<0) x = width;
+		if(y<0) y = height;
+		if(x>width) x = 0;
+		if(y>height) y = 0;
+	}
+
+	function parseResult()
+	{
+		// recognition system will often append words into phrases.
+		// so hack here is to only use the last word:
+		var mostrecentword = myRec.resultString.split(' ').pop();
+		if(mostrecentword.indexOf("left")!==-1) { dx=-1;dy=0; }
+		else if(mostrecentword.indexOf("right")!==-1) { dx=1;dy=0; }
+		else if(mostrecentword.indexOf("up")!==-1) { dx=0;dy=-1; }
+		else if(mostrecentword.indexOf("down")!==-1) { dx=0;dy=1; }
+		else if(mostrecentword.indexOf("clear")!==-1) { background(255); }
+		console.log(mostrecentword);
+	}
